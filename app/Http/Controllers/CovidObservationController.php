c037b8f9fcd2097d695fbad04858ecca98c24943
Paulo Basilio <paulolorenzobasilio@yahoo.com>
2020-06-13 13:12:28 +0800

    some code cleaning

--- app/Http/Controllers/CovidObservationController.php
+++ app/Http/Controllers/CovidObservationController.php
@@ -2,43 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CovidObservation;
 use Illuminate\Support\Facades\DB;
 
 class CovidObservationController extends Controller
 {
+    private $covidObservation;
+
+    public function __construct(CovidObservation $covidObservation)
+    {
+        $this->covidObservation = $covidObservation;
+    }
+
     public function __invoke()
     {
-        $covidObservations =  DB::table('covid_observations')
-            ->select(
-                [
-                    'observation_date', 'country', DB::raw('SUM(confirmed) as confirmed'),
-                    DB::raw('SUM(deaths) as deaths'), DB::raw('SUM(recovered) as recovered')
-                ]
-            )
-            ->when(request('observation_date', false), function ($q, $observationDate) {
-                return $q->whereObservationDate($observationDate);
-            })
-            ->groupBy(['observation_date', 'country'])
+        $covidObservations =  $this->covidObservation->select([
+            'observation_date', 'country', DB::raw('SUM(confirmed) as confirmed'),
+            DB::raw('SUM(deaths) as deaths'), DB::raw('SUM(recovered) as recovered')
+        ])->when(request('observation_date', false), function ($q, $observationDate) {
+            return $q->whereObservationDate($observationDate);
+        })->groupBy(['observation_date', 'country'])
             ->orderByDesc(DB::raw('SUM(confirmed)'))
             ->limit(request()->query('max_results', 15))
             ->get();
 
         if (request('observation_date', false)) {
-            return $this->buildSingleResult($covidObservations);
+            return $this->buildSingleResultSet($covidObservations);
         }
 
-        return $this->buildMultipleResult($covidObservations);
+        return $this->buildMultipleResultSet($covidObservations);
     }
 
     /**
-     * Construct the structure by
+     * Construct the structure by object
      * {
-     * observation_date: "2020-01-22"
-     * countries: [...]
+     *      observation_date: "2020-01-22"
+     *      countries: [...]
      * }
      */
-    private function buildSingleResult($data)
+    private function buildSingleResultSet($data)
     {
+        $results = [];
         foreach ($data as $val) {
             $results["observation_date"] = $val->observation_date;
             unset($val->observation_date);
@@ -48,24 +52,21 @@ class CovidObservationController extends Controller
     }
 
     /**
-     * Construct the structure by multiple set
+     * Construct the structure by array of singleResultSet
      * {
      *      [
      *          observation_date: "2020-01-22"
      *          countries: [...]
      *      ]
-     *      [
-     *          observation_date: "2020-01-23"
-     *          countries: [...]
-     *      ]
+     *      [...]
      * }
      */
-    private function buildMultipleResult($data)
+    private function buildMultipleResultSet($data)
     {
         $results = [];
         $data = $data->groupBy('observation_date');
-        foreach ($data as $key => $val) {
-            $results [] = $this->buildSingleResult($val);
+        foreach ($data as $val) {
+            $results[] = $this->buildSingleResultSet($val);
         }
         return $results;
     }
